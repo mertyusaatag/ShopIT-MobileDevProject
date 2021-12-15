a231b486095988c5dba95b2c10855417c1bc943c
J-Sob <80786093+J-Sob@users.noreply.github.com>
2021-12-15 20:03:07 +0100

    Delete routes directory

--- routes/UserRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {create,index,login} = require("../controllers/User")
-
-const router = express.Router();
-
-router.post("/",create);
-
-module.exports= router;
