4831f11f1493c42dcdd12c8a1f9548d1ba48d486
J-sob <sobczynski.kuba@wp.pl>
2021-12-15 19:59:22 +0100

    moved files to server-side dir

---
+++ server-side/config/index.js
@@ -0,0 +1,8 @@
+const server = require("./server")
+const mongoose = require("mongoose")
+
+module.exports =()=>{
+   
+    server();
+  
+}
---
+++ server-side/controllers/User.js
@@ -0,0 +1,27 @@
+const {insert , list , loginUser , UniqueEmail} = require("../services/User");
+const httpStatus = require("http-status");
+const { passwordToHash } = require("../utils/helper");
+
+const create = async (req,res) =>
+{
+  try {
+    const usermail = req.body.email;
+    const existUser = await UniqueEmail(usermail);
+    console.log(existUser);
+    if(existUser) throw new Error("user already exist")
+    req.body.password = passwordToHash(req.body.password);
+    response = await insert(req.body);
+    res.status(httpStatus.CREATED).send(response);
+  } catch (error) {
+      console.log(error)
+    res.status(httpStatus.BAD_REQUEST).send(error.message)
+  }
+       
+    }
+ 
+    
+
+
+module.exports = {
+    create,
+}
