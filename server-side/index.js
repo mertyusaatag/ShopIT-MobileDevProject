df8b84021aa69909183689c0cfeb07d3f445d65b
unknown <ul0249604@edu.uni.lodz.eu>
2021-12-22 22:27:29 +0100

    first back of orders

--- server-side/index.js
+++ server-side/index.js
@@ -7,6 +7,7 @@ const helmet = require("helmet")
 const config = require("./config/index")
 const {UserRoutes} = require("./routes");
 const loaders = require("./loaders");
+const orderRoute = require("./routes/OrderRouter");
 
 
 config();
@@ -16,7 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-
+app.use("/Order", orderRoute);
 
 // API endpoints
 
--- server-side/routes/UserRouter.js
+++ server-side/routes/UserRouter.js
@@ -5,4 +5,4 @@ const router = express.Router();
 
 router.post("/",create);
 
-module.exports= router;
+module.exports = router;
